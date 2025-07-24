dcbaff3b2f9fa7d4bc683e4eec2cb8e609ee175e
Congbang <bangpham10062002@gmail.com>
2025-07-24 08:37:07 +0700

    changechange

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const declineBtn = document.querySelector(".decline");
   const acceptBtn = document.querySelector(".accept");
+  const thinkBtn = document.querySelector(".think");
   const message = document.querySelector(".message");
   const buttons = document.querySelector(".buttons");
   const heading = document.querySelector("h1"); // "Dear Thanh Nguyên"
@@ -28,5 +29,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     message.innerHTML =
       '<p class="accept-message">Cảm ơn bạn vì đã đồng ý nha, bạn có thể đi lúc nào thì nói cho tui biết nhé 😚</p>';
+  });
+
+  // Khi bấm "Cho tui suy nghĩ"
+  thinkBtn.addEventListener("click", function () {
+    heading.style.display = "none";
+    buttons.style.display = "none";
+
+    let countdown = 5;
+    message.innerHTML = `<p class="thinking-message">Cho bạn suy nghĩ ${countdown} giây...</p>`;
+
+    const interval = setInterval(() => {
+      countdown--;
+      if (countdown > 0) {
+        message.innerHTML = `<p class="thinking-message">Cho bạn suy nghĩ ${countdown} giây...</p>`;
+      } else {
+        clearInterval(interval);
+        message.innerHTML = `<p class="thinking-message">Đã hết thời gian suy nghĩ, lựa chọn lại đi nha 😗</p>`;
+
+        setTimeout(() => {
+          heading.style.display = "block";
+          message.innerHTML = originalMessage;
+
+          // Ẩn nút "Cho tui suy nghĩ", chỉ còn 2 nút
+          thinkBtn.style.display = "none";
+          buttons.style.display = "flex";
+        }, 2000);
+      }
+    }, 1000);
   });
 });
